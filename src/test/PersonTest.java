5164adeb5c60fe37a5e0cdbe7830b33d8aee062e
Andrew Stevens <andrew.stevens@pnnl.gov>
2012-12-13 12:38:33 -0800

    initial commit

---
+++ src/test/PersonTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PersonTest {
+    @Test
+    public void canConstructAPersonWithAName() {
+        Person person = new Person("Larry");
+        assertEquals("Larry", person.getName());
+    }
+}
