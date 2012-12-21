7506854f06c8c2634e54e44a8d192eee7698307a
Andrew Stevens <andrew.stevens@pnnl.gov>
2012-12-20 17:06:08 -0800

    first problem!: populate the Person.name field

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,13 +1,8 @@
-package tcpl;
-
-import org.apache.commons.collections.list.GrowthList;
-
 public class Person {
     private final String name;
 
     public Person(String name) {
-        this.name = name;
-        new GrowthList();
+        
     }
 
     public String getName() {
