b79ec34073c55f2b2b6e6f026c4fec8aaad9f16b
Chandler May <chandler.may@pnnl.gov>
2013-04-25 13:06:48 -0700

    add main soln

--- src/SocialNetwork.java
+++ src/SocialNetwork.java
@@ -1,6 +1,10 @@
 import java.awt.Dimension;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Scanner;
+
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.JFrame;
 
@@ -14,10 +18,17 @@ public class SocialNetwork {
     private Set<Connection> connections;
     private Set<String> nodes;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SocialNetwork network = new SocialNetwork();
 
-        throw new UnsupportedOperationException("Finish me!");
+        Scanner scanner = new Scanner(new File(args[0]));
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            Scanner lineScanner = new Scanner(line);
+            String from = lineScanner.next();
+            String to = lineScanner.next();
+            network.add(new Connection(from, to));
+        }
 
         network.visualize();
     }
